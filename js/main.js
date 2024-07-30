1617e065bde8ec9349a841fc99ad77e8d20ba5d2
YEOJEONGS2 <yoog1219@gmail.com>
2024-07-30 17:21:39 +0900

    키친 카테고리
    공통 영역, 카테고리 배너 하단 왼쪽 텍스트 상자 스타일 적용 / 오른쪽 스와이퍼 상품리스트 적용중

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 window.addEventListener("load",function () {
     var swpr = new Swiper(".productSlide", {
-    slidesPerView: 2,
-    spaceBetween: 30,
+    slidesPerView: 3,
+    spaceBetween: 10,
     // centeredSlides: true,
     autoplay: {
     delay: 2500,
     disableOnInteraction: false,
-    },
-    pagination: {
-    el: ".productSlide .swiper-pagination",
-    clickable: true,
     },
   });
 });
