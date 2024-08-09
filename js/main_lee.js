af12fe33bbdd1e71b243937380033d8ca502bc60
jubong2 <twoweekyear2@gmail.com>
2024-08-09 12:00:50 +0900

    모바일 navi button 생성

--- js/main_lee.js
+++ js/main_lee.js
@@ -22,6 +22,38 @@ window.addEventListener("load", function () {
       disableOnInteraction: false,
     },
     loop: true,
-    breakpoints: { 1400: { slidesPerView: 4 }, 1100: {slidesPerView: 3 }, 700: { slidesPerView: 2}, 601: { slidesPerView: 2 } },
+    breakpoints: { 1400: { slidesPerView: 4 }, 1100: { slidesPerView: 3 }, 700: { slidesPerView: 2 }, 601: { slidesPerView: 2 } },
+  });
+
+  // 모바일 메뉴바 클릭하면 메뉴 나오는거
+  const mbNavi = document.querySelector(".mb-navi");
+  const mbBtn = document.querySelector(".mb-menu");
+  const htmlRoot = document.querySelector("html");
+  const mbmenubarBtn = this.document.querySelector(".mb-menu-bar");
+
+  mbBtn.addEventListener("click", function () {
+    // 현재 버튼에 'mbactive' 클래스가 있는지 확인합니다.
+    const mbActive = mbBtn.classList.contains("mbactive");
+    if (mbActive) {
+      // 'mbactive' 클래스가 있으면 제거합니다.
+      mbBtn.classList.remove("mbactive");
+      mbNavi.classList.remove("mbactive");
+      htmlRoot.classList.remove("mbactive");
+    } else {
+      // 'mbactive' 클래스가 없으면 추가합니다.
+      mbBtn.classList.add("mbactive");
+      mbNavi.classList.add("mbactive");
+      htmlRoot.classList.add("mbactive");
+    }
+  });
+  mbmenubarBtn.addEventListener("click", function () {
+    const mbActive = mbBtn.classList.contains("mbactive");
+    if (mbActive) {
+      mbmenubarBtn.classList.remove("mbactive");
+      mbNavi.classList.remove("mbactive");
+    } else {
+      mbmenubarBtn.classList.add("mbactive");
+      mbNavi.classList.add("mbactive");
+    }
   });
 });
